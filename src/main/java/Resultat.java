817ea3215f53f8dff402fb20e86f9ea55b30d055
Théo <theo.cholley@gmail.com>
2018-04-10 16:13:30 +0200

    Init

---
+++ src/main/java/Resultat.java
@@ -0,0 +1,10 @@
+package main.java;
+
+import java.io.Serializable;
+
+public class Resultat implements Serializable {
+    @Override
+    public String toString() {
+        return "Salut Thib";
+    }
+}
